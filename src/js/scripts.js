c5c012e5527507bf5dffde336f102dc942650a17
Arv <pandit.arv@gmail.com>
2022-04-03 19:49:36 -0600

    Control flow and validation of first name and last name;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,42 @@
 async function main() {
   // This is where the code you're actually experimenting with goes.
 
-  let menuOption = 0;
+  let menuOption;
   do {
     output("1- Create New Car Profile \n2- View Previous Profile \n3- Quit Application");
 
     menuOption = Number(await input("Please Enter 1, 2 or 3 to select your option: "));
 
+    const custInfo = [];
+
     switch (menuOption) {
       case 1:
-        // let firstNameInput = await input('Please Enter your First Name: ')
-        output("You are in Creat New Car Profile");
+        // First Name
+        let firstNameInput;
+        do {
+          firstNameInput = await input("Please Enter your First Name:  ");
+          if (checkName(firstNameInput)) {
+            custInfo.push(firstNameInput);
+          }
+        } while (!checkName(firstNameInput));
+
+        let lastNameInput;
+        do {
+          // First Name
+          lastNameInput = await input("Please Enter your Last Name:  ");
+          if (checkName(lastNameInput)) {
+            custInfo.push(lastNameInput);
+          }
+        } while (!checkName(lastNameInput));
+
+        output(custInfo);
+        // if () {
+        //   output(`matches the Regular Expression`);
+        //   // Last Name
+        //   let lastNameInput = await input("Please Enter your Last Name: ");
+        // } else {
+        //   output("try again");
+        // }
         break;
       case 2:
         output(" You are in View Previous Profile");
@@ -21,5 +47,10 @@ async function main() {
       default:
         output("Please select the valid option.");
     }
-  } while (menuOption != 4);
+  } while (menuOption != 3);
+}
+
+function checkName(inputVal) {
+  const regEx = /^[A-Za-z-]{1,20}$/;
+  return regEx.test(inputVal);
 }
