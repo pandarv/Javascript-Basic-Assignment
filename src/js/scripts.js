be28bd0c0134b988882acc387c30708ca480a230
Arv <pandit.arv@gmail.com>
2022-04-17 10:07:03 -0600

    Refactored under 40 lines

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,4 @@
 async function main() {
-  // This is where the code you're actually experimenting with goes.
   const customerDatabase = [];
   let menuOption;
   do {
@@ -8,45 +7,25 @@ async function main() {
     const customerInfo = [];
     switch (menuOption) {
       case 1:
-        // let inputDetail = [
-        //   [firstNameInput, /^[a-z-]{1,20}$/i],
-        //   [lastNameInput, /^[a-z-]{1,20}$/i],
-        //   [addressInput, /^([a-z#-,.\d]( ){0,}){5,50}$/i],
-        //   [carModel, /^([a-z\d]( ){0,}){1,15}$/i],
-        //   [VINInput, /^[A-Z\d]{17}$/i],
-        // ];
-        let firstNameInput;
-        while (!(firstNameInput = await input("Please Enter your First Name:  ")).match(/^[a-z-]{1,20}$/i)) {}
-        customerInfo.push(firstNameInput);
-        let lastNameInput;
-        while (!(lastNameInput = await input("Please Enter your Last Name:  ")).match(/^[a-z-]{1,20}$/i)) {}
-        customerInfo.push(lastNameInput);
-        let addressInput;
-        while (!(addressInput = await input(`Please Enter your Address (Between 5-50 with special characters #,-.):  `)).match(/^([a-z#-,.\d]( ){0,}){5,50}$/i)) {}
-        customerInfo.push(addressInput);
-        let purchaseDateInput;
-        while (!isDateValid((purchaseDateInput = await input("Please Enter Date of Purchase in yyyy-mm-dd format: ")))) {}
-        customerInfo.push(purchaseDateInput);
-        let carBrandInput;
-        while (!checkBrand((carBrandInput = await input("Please Enter your Car Brand: ")))) {}
-        customerInfo.push(carBrandInput);
-        let carModel;
-        while (!(carModel = await input("Please Enter your car Model: ")).match(/^([a-z\d]( ){0,}){1,15}$/i)) {}
-        customerInfo.push(carModel);
-        let carYear;
-        while (!isYearValid((carYear = await input("Please Enter Car Make Year: ")))) {}
-        customerInfo.push(carYear);
-        let VINInput;
-        while (!(VINInput = await input("Please Enter 17 character long VIN Number: ")).match(/^[A-Z\d]{17}$/i)) {}
-        customerInfo.push(VINInput.toUpperCase());
+        let inputDetail = [
+          [`Please Enter your First Name: `, (x) => !/^[a-z-]{1,20}$/i.test(x)],
+          [`Please Enter your Last Name:  `, (x) => !/^[a-z-]{1,20}$/i.test(x)],
+          [`Please Enter your Address (Between 5-50 with special characters #,-.):  `, (x) => !/^([a-z#-,.\d]( ){0,}){5,50}$/i.test(x)],
+          [`Please Enter Date of Purchase in yyyy-mm-dd format: `, (x) => !/^(199[0-9]|200\d|201\d|202[0-2])-(02-(0[1-9]|[12][0-9])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))$/.test(x)],
+          [`Please Enter your Car Brand: `, (x) => !["bmw", "gm", "honda", "toyota", "audi", "kia"].includes(x.toLowerCase())],
+          [`Please Enter your Car Model: `, (x) => !/^([a-z\d]( ){0,}){1,15}$/i.test(x)],
+          [`Please Enter Car Make Year: `, (x) => !/^(199[0-9]|200\d|201\d|202[0-3])$/.test(x)],
+          [`Please Enter 17 character long VIN Number: `, (x) => !/^[A-Z\d]{17}$/i.test(x)],
+        ];
+        for (item of inputDetail) {
+          while (item[1]((userInput = (await input(item[0])).trim())));
+          customerInfo.push(userInput.toUpperCase());
+        }
         customerDatabase.push(customerInfo);
         break;
       case 2:
         for (let [i, row] of customerDatabase.entries()) {
-          output(`\nCustomer #${i + 1}`);
-          for (let item of row) {
-            output(`       ${item}`);
-          }
+          output(`\nCustomer #${i + 1}\n   First Name:${row[0]}\n   Last Name:${row[1]}\n   Address:${row[2]}\n   Purchase Date:${row[3]}\n   Car Brand:${row[4]}\n   Car Model:${row[5]}\n   Car Year:${row[6]}\n   Car VIN Number:${row[7]}`);
         }
         break;
       case 3:
@@ -56,40 +35,4 @@ async function main() {
         output("Please select the valid option.");
     }
   } while (menuOption != 3);
-}
-// let checkVal = (inputVal, regEx) => regEx.test(inputVal);
-
-function checkBrand(inputVal) {
-  const carBrand = ["bmw", "gm", "honda", "toyota", "audi", "kia"];
-  let outputVal = false;
-
-  for (item of carBrand) {
-    if (inputVal.toLowerCase().includes(item)) {
-      outputVal = true;
-    }
-  }
-  return outputVal;
-}
-
-function isYearValid(inputVal) {
-  return Number(inputVal) >= 1990 && Number(inputVal <= new Date().getFullYear() + 1);
-}
-
-function isDateValid(inputVal) {
-  let outputVal = true;
-  let dateValue = inputVal.split("-");
-  // Year Validation
-  if (dateValue[0] > new Date().getFullYear()) {
-    outputVal = false;
-  }
-  //  Month Validation
-  if (dateValue[1] > 12 || dateValue[1] < 1 || !Number.isInteger(Number(dateValue[1]))) {
-    outputVal = false;
-  }
-
-  // Date validation
-  if (dateValue[2] > 31 || dateValue[2] < 1 || !Number.isInteger(Number(dateValue[2]))) {
-    outputVal = false;
-  }
-  return outputVal;
 }
